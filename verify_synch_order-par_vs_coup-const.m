1fb8e0e01a3619bec9e33f194abbc2385e8a6bac
stutipathak5 <92648447+stutipathak5@users.noreply.github.com>
2021-10-19 13:37:58 +0530

    Update verify_synch_order-par_vs_coup-const.m

--- verify_synch_order-par_vs_coup-const.m
+++ verify_synch_order-par_vs_coup-const.m
@@ -1,7 +1,7 @@
 global N omega a
 
-% select which to verify erdos-reyni("er") or scale free("sf") graph
-graph_type= "er"
+% select which to verify and plot: erdos-reyni("er") or scale free("sf") graph
+graph_type= "er";
 
 if graph_type== "er"
 a=load('ER+SF_New/er60.txt');
@@ -9,26 +9,24 @@ else
 a=load('ER+SF_New/sf60.txt');
 end
 
-N=size(a,1)
+N=size(a,1);
 omega=generate_random(0.5,N);
 y0=(generate_random(pi,N))';
 tspan=0:0.01:400;
 rr2=[];
 m=10000;
 
-lamda=0
-lam=[]
+lamda=0;
+lam=[];
 for k=1:60
     del_lamda=0.0025;
-    lamda=(k-1)*del_lamda
+    lamda=(k-1)*del_lamda;
     [t,y]=ode45(@(t,y)odefm(t,y,lamda),tspan,y0);
-    size(y)
     y=wrapToPi(y);
     sz1=size(y,1);
-    r= order_par(y((sz1-m):sz1,:))
+    r= order_par(y((sz1-m):sz1,:));
     rr2=[rr2,r];
-    lam=[lam;lamda]
-    disp(k);
+    lam=[lam;lamda];
 end
 set(0, 'DefaultAxesFontSize', 14)
 plot(lam,rr2,'-o')
@@ -40,12 +38,10 @@ else
 title('scale free (N=100, <k>=18)')
 end
 
-
 function theta_dot = odefm(~,theta,lamda)
      global omega a
      theta_dot = omega + lamda*sum(a.*sin(theta-theta'))';
 end
-
 
 function rr=generate_random(range,no_points)
 rr=range*rand(no_points,1);
